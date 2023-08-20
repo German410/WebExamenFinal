f21449c540ae016c28a7be11b62686bd00cd5399
German <german410@hotmail.com>
2023-08-20 15:43:25 -0500

    Agregar archivos de proyecto.

---
+++ WebExamenFinal/Models/DenunciasModel.cs
@@ -0,0 +1,14 @@
+﻿namespace WebExamenFinal.Models
+{
+    public class DenunciasModel
+    {
+        public int idDenuncia { get; set; }
+        public string? dni { get; set; }
+        public string? Nombre { get; set; }
+        public string? Apellidos { get; set; }
+        public string? Empresa { get; set; }
+        public string? Ciudad { get; set; }
+        public string? Telefono { get; set; }
+        public string? Denuncia { get; set; }
+    }
+}
---
+++ WebExamenFinal/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebExamenFinal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
