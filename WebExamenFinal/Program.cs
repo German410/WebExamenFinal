42f19393d5955b60ae909de0727eda3c185ace11
German <german410@hotmail.com>
2023-08-20 16:57:39 -0500

    add view

--- WebExamenFinal/Program.cs
+++ WebExamenFinal/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Denuncias}/{action=Index}/{id?}");
 
 app.Run();
