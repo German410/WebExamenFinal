41e8b6ddec03284dec4cf5dba36bb4de96e79b0b
German <german410@hotmail.com>
2023-08-20 15:50:39 -0500

    controller

---
+++ WebExamenFinal/Controllers/DenunciaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebExamenFinal.Controllers
+{
+    public class DenunciaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
