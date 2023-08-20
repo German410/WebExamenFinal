2f9b6db8a62787d6bd03f3a880eb8c4488261883
German <german410@hotmail.com>
2023-08-20 16:43:21 -0500

    add method controller

--- WebExamenFinal/Controllers/DenunciaController.cs
+++ WebExamenFinal/Controllers/DenunciaController.cs
@@ -1,12 +1,40 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebExamenFinal.Models;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using System.Net;
+using System.Security.Principal;
 
 namespace WebExamenFinal.Controllers
 {
     public class DenunciaController : Controller
     {
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+
+            List<DenunciasModel> denunciasList = new List<DenunciasModel>();
+
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.BaseAddress = new Uri("https://localhost:7297/api/Denuncias/");
+                HttpResponseMessage response = await httpClient.GetAsync("GetDenuncias");
+                string apirResponse = await response.Content.ReadAsStringAsync();
+                denunciasList = JsonConvert.DeserializeObject<List<DenunciasModel>>(apirResponse).Select(
+                s => new DenunciasModel
+                {
+                        idDenuncia = s.idDenuncia,
+                        dni = s.dni,
+                        Nombre = s.Nombre,
+                        Apellidos = s.Apellidos,
+                        Empresa = s.Empresa,
+                        Ciudad = s.Ciudad,
+                        Telefono = s.Telefono,
+                        Denuncia = s.Denuncia
+                    }  
+                    ).ToList();
+            }
+
+                return View(denunciasList);
         }
     }
 }
